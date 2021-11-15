26ccecee9e3ad1685e1afb84839504392db4eb5a
Jong jae Park <pjj186@naver.com>
2021-11-16 00:35:49 +0900

    모드 변경에 따라 리스트 다르게 보여주기
    Work 또는 Travel을 선택하고 리스트를 작성했을때, 오브젝트에 저장된 working값과 현재 앱에 설정된 working값을 비교하여 구현.

--- App.js
+++ App.js
@@ -25,7 +25,7 @@ export default function App() {
       return;
     }
 
-    const newToDos = { ...toDos, [Date.now()]: { text, work: working } };
+    const newToDos = { ...toDos, [Date.now()]: { text, working } };
     // save to do
     setToDos(newToDos);
     setText("");
@@ -58,11 +58,13 @@ export default function App() {
         style={styles.input}
       />
       <ScrollView>
-        {Object.keys(toDos).map((key) => (
-          <View style={styles.toDo} key={key}>
-            <Text style={styles.toDoText}>{toDos[key].text}</Text>
-          </View>
-        ))}
+        {Object.keys(toDos).map((key) =>
+          toDos[key].working === working ? (
+            <View style={styles.toDo} key={key}>
+              <Text style={styles.toDoText}>{toDos[key].text}</Text>
+            </View>
+          ) : null
+        )}
       </ScrollView>
     </View>
   );
